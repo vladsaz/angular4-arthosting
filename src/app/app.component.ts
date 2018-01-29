fadf3ef82ebe72cd3750a8f017b3917ad81160f7
Vladyslav Sazonov <vlad.sazonov@naturalint.com>
2018-01-29 09:18:35 +0200

    video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  imageUrl = undefined;
+  videoUrl = undefined;
+  urlCreator = window.URL;
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {
+  }
+
+  getData() {
+    this.http.get('http://localhost:5000/angular', { responseType: "blob" }).subscribe(data => {
+      // console.log(data);
+      this.imageUrl = this.sanitizer.bypassSecurityTrustUrl(this.urlCreator.createObjectURL(data));
+      // console.log(this.imageUrl);
+    });
+  }
+
+  getDataVideo() {
+    this.http.get('http://localhost:5000/video', { responseType: "blob" }).subscribe(data => {
+      console.log(data);
+      this.videoUrl = this.sanitizer.bypassSecurityTrustUrl(this.urlCreator.createObjectURL(data));
+      // console.log(this.videoUrl);
+    });
+  }
 }
